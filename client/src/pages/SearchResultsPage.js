1f6daa5a37b4fd4b1b4cbbaddb6a98bb8c288933
AveryNewhart <newhartreeceavery@gmail.com>
2023-06-17 16:13:39 -0400

    modal appearing when user clicks button to add a review, need to do backend work to get it fully working

--- client/src/pages/SearchResultsPage.js
+++ client/src/pages/SearchResultsPage.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from "../components/Navigation.js";
 // import axios from 'axios';
 import { useMutation } from '@apollo/client';
 import { SAVE_TO_WANNA_LISTEN, SAVE_TO_LISTENED } from "../utils/mutations";
 import "../styles/SearchForm.css"
+import '../styles/SearchResultsPage.css'; // Add this line
 
 
 const SearchResultsPage = () => {
 
 const [saveToListened] = useMutation(SAVE_TO_LISTENED);
 const [saveToWannaListen] = useMutation(SAVE_TO_WANNA_LISTEN);
+
+const [isModalOpen, setIsModalOpen] = useState(false);
+const [reviewText, setReviewText] = useState('');
+
 
     // Retrieve the search results from the query parameters
   const searchResults = JSON.parse(new URLSearchParams(window.location.search).get('searchResults'));
@@ -62,29 +67,73 @@ if (!searchResults || searchResults.length === 0) {
       }
   };
 
-  return (
-    <div>
-      <Navigation />
+  const submitReview = () => {
+    // Perform any necessary actions with the review text
+    console.log('Review submitted:', reviewText);
+  
+    // Close the modal
+    closeModal();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setReviewText('');
+  };
+  
+
+    return (
+        <div>
+          <Navigation />
           <h1>Search Results</h1>
-      {searchResults.map((album, index) => (
-     <div key={index} className="albumInside">
-     <p>Artist: {album.artistName}</p>
-     <img src={album.albumPic} alt="" className='coverArt'/>
-     <p>Album: {album.albumName}</p>
-     <p>Release Date: {album.releaseDate}</p>
-     <div>
-        <button variant="primary" className="" onClick={() => handleSaveToListened(album)}>
-              Save to Listened
-            </button>
-        <button variant="primary" className="" onClick={() => handleSaveToWannaListen(album)}>
-              Save to Wanna Listen
-            </button>
-     </div>
-     <button className='reviewBtn'>Add Review</button>
-   </div>
-      ))}
-    </div>
-  );
-};
+          {searchResults.map((album, index) => (
+            <div key={index} className="albumInside">
+              <p>Artist: {album.artistName}</p>
+              <img src={album.albumPic} alt="" className="coverArt" />
+              <p>Album: {album.albumName}</p>
+              <p>Release Date: {album.releaseDate}</p>
+              <div>
+                <button
+                  variant="primary"
+                  className=""
+                  onClick={() => handleSaveToListened(album)}
+                >
+                  Save to Listened
+                </button>
+                <button
+                  variant="primary"
+                  className=""
+                  onClick={() => handleSaveToWannaListen(album)}
+                >
+                  Save to Wanna Listen
+                </button>
+              </div>
+              <button className="reviewBtn" onClick={openModal}>
+                Add Review
+              </button>
+            </div>
+          ))}
+          {isModalOpen && (
+            <div className="modal">
+              <div className="modal-content">
+                <h2>Add Review</h2>
+                <textarea
+                  value={reviewText}
+                  onChange={(e) => setReviewText(e.target.value)}
+                  rows={4}
+                  cols={50}
+                />
+                <button onClick={submitReview}>Submit</button>
+                <button onClick={closeModal}>Cancel</button>
+              </div>
+            </div>
+          )}
+        </div>
+      );
+          };
+      
 
 export default SearchResultsPage;
