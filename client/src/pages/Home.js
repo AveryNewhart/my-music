c8e5e5f49bd360b435c1777bd8cba485b5e531e1
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-02 17:28:23 -0400

    homepage text added and positioned, just need to decide on color again haha

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,18 @@
 import React from 'react'
 // import "../styles/App.css"
 import "../styles/Home.css"
-// import Navigation from "../components/Navigation"
+import Navigation from "../components/Navigation"
  
 export default function Home() {
   return (
-    <div className=''>
-      {/* <Navigation /> */}
-        <h1>My Music</h1>
-        <a href="/login" className="login">
-              Login/Create
-          </a>
-          <br></br>
-          <a href="/profile" className="login">
-              View Profile
-          </a>
-          <br></br>
-          <a href="/feed" className="login">
-              View Feed
-          </a>
-      
-    </div>
+    <>
+     <Navigation />
+    <div className='home-container'>
+    <h1 className='home-head'>Welcome to My Music</h1>
+    <h3 className='home-head-3'> Discover, save, and share your favorite music with My Music.</h3>
+    <p className='home-subtitle'>Explore their albums, save your favorites, write reviews, and connect with other music lovers.</p>
+    <p className='home-subtitle'>My Music is the ultimate platform for music discovery and sharing.</p>
+  </div>
+  </>
   )
 }
